6899a3ab8c89cbf83dac0f382ee6bd61d3fd66fb
Charles <charles.lamarque69@gmail.com>
2023-03-01 19:02:43 +0100

    Add tests for calculator

--- functions.test.js
+++ functions.test.js
@@ -12,4 +12,21 @@ test('capitalize, some lower', () => {
 // reverseString tests
 test('reverse string', () => {
     expect(reverseString('hello')).toBe('olleh');
-})
+});
+
+// calculator tests
+test('calculator: add', () => {
+    expect(calculator.add(10, 5)).toBe(15);
+});
+
+test('calculator: substract', () => {
+    expect(calculator.substract(10, 5)).toBe(5);
+});
+
+test('calculator: divide', () => {
+    expect(calculator.divide(1, 2)).toBeCloseTo(0.5);
+});
+
+test('calculator: multiply', () => {
+    expect(calculator.multiply(3, 7)).toBe(21);
+});
