a3d23a9b6bc270d4688f66f59946e08ab66c6b76
Charles <charles.lamarque69@gmail.com>
2023-03-01 18:51:09 +0100

    Add reverseString test

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,15 @@
 import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./functions"; 
 
+// CAPITALIZE TESTS
 test('capitalize, all lower', () => {
     expect(capitalize('hello').toBe('HELLO'));
 });
 
 test('capitalize, some lower', () => {
     expect(capitalize('CounTry').toBe('COUNTRY'));
+});
+
+// REVERSESTRING TESTS
+test('reverse string', () => {
+    expect(capitalize('hello').toBe('olleh'));
 });
