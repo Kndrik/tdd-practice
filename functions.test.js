a4bff7cf277a40e143cc59a84403133cf861f545
Charles <charles.lamarque69@gmail.com>
2023-03-01 19:30:36 +0100

    Add analyze array test

--- functions.test.js
+++ functions.test.js
@@ -39,3 +39,13 @@ test('caesar cipher', () => {
 test('caesar cipher', () => {
     expect(caesarCipher('young', 5)).toBe('dtzsl');
 });
+
+// analyze array test
+test('analyze array', () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+    });
+});
