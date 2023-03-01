49a0d4c42ba5b39b6f88879e6ab76b529941f7f4
Charles <charles.lamarque69@gmail.com>
2023-03-01 18:50:19 +0100

    Add capitalize tests

--- functions.test.js
+++ functions.test.js
@@ -1,1 +1,9 @@
-import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./functions";
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./functions"; 
+
+test('capitalize, all lower', () => {
+    expect(capitalize('hello').toBe('HELLO'));
+});
+
+test('capitalize, some lower', () => {
+    expect(capitalize('CounTry').toBe('COUNTRY'));
+});
