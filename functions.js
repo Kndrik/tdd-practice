e3c05d4c122a2047c38c4289787831e377b53d03
Charles <charles.lamarque69@gmail.com>
2023-03-01 19:03:34 +0100

    Complete calculator functions

--- functions.js
+++ functions.js
@@ -8,19 +8,19 @@ export function reverseString(input) {
 
 export const calculator = (() => {
     const add = (x, y) => {
-
+        return x + y;
     }
 
     const substract = (x, y) => {
-
+        return x - y;
     }
 
     const divide = (x, y) => {
-
+        return x / y;
     }
 
     const multiply = (x, y) => {
-
+        return x * y;
     }
 
     return { add, substract, divide, multiply }
